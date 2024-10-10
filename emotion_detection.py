0e4dbba8b6fa66b6a6debe17d605566f127cf401
cooperurbec <cooperurbec@gmail.com>
2024-10-10 17:32:42 -0400

    next task  4

---
+++ emotion_detection.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = header)
+    formated_response = json.loads(response.text)
+    emotions = formated_response['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion = ""
+    dominant_emotion_value = 0.0
+
+    for emotion in emotions:
+        if emotions[emotion] > dominant_emotion_value:
+            dominant_emotion = emotion
+            dominant_emotion_value = emotions[emotion]
+
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
